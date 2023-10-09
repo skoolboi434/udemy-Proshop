c847fce76849ae4a5a1e7d90d81ad270289407a3
Travis Davis <skoolboi434@gmail.com>
2023-10-09 15:20:36 -0400

    Fetching products and product details through redux

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('api/products');
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map(product => (
+              <Col key={product._id} sm={12} md={6} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
